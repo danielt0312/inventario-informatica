c793be6f845fa9f435d6431e262925875e7c00b4
danielt0312 <torrescoloradodaniel@gmail.com>
2026-02-06 13:40:27 -0600

    delete breakpoints to container's padding

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,13 +3,13 @@ import Logo from "./Logo"
 function Header() {
     return (
         <div className="bg-white flex justify-center">
-            <div className="container py-5">
+            <div className="container py-5 w-full">
                 <div className="flex flex-row items-center justify-between">
-                    <Logo />
-                    <div className="text-3xl font-black text-center">
+                    <Logo className="h-18" />
+                    <div className="text-3xl font-bold text-center">
                         Inventario Informática
                     </div>
-                    <Logo className="opacity-0" />
+                    <Logo className="h-18 opacity-0" />
                 </div>
             </div>
         </div>
