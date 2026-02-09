bac5fec1500e3d6723976f88c4f206bcabe8317d
danielt0312 <torrescoloradodaniel@gmail.com>
2026-02-09 09:45:36 -0600

    update content height to grow to the parent layout

--- frontend/src/components/AuthLayout.tsx
+++ frontend/src/components/AuthLayout.tsx
@@ -5,15 +5,15 @@ import Footer from "./Footer";
 
 function AuthLayout() {
     return (
-        <div className="bg-gray-100 min-h-screen">
+        <div className="flex flex-col min-h-screen bg-gray-100">
             <Header />
             <Navbar />
 
-            <div className="flex justify-center">
+            <main className="flex-1 flex justify-center">
                 <div className="container py-4 flex flex-col gap-4">
                     <Outlet />
                 </div>
-            </div>
+            </main>
 
             <Footer />
         </div>
