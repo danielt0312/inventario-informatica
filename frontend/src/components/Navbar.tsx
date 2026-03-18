2461d4871b0d0b7c242270ab70497be10ac94c9b
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-03-18 11:42:35 -0600

    add authenticated User name to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import { Link } from "@tanstack/react-router"
 import NavbarItem from "./NavbarItem"
+import { Route as AuthRoute } from "@/routes/_auth";
+import { Route as InventarioRoute } from "@/routes/_auth/inventario";
+import { Route as DictamenRoute } from "@/routes/_auth/dictamen";
 
-// todo: verify UserRole to display them
 function Navbar() {
+    const { user } = AuthRoute.useRouteContext()
+
     return (
-        <div className="bg-stone-900 flex justify-center">
-            <div className="container">
-                <Link to="/inventario"><NavbarItem>Inventario</NavbarItem></Link>
-                <Link to="/dictamen"><NavbarItem>Dictamen Tecnológico</NavbarItem></Link>
+        <div className="bg-stone-900">
+            <div className="container flex flex-row">
+                <div className="flex-1">
+                    <Link to={InventarioRoute.to}><NavbarItem>Inventario</NavbarItem></Link>
+                    <Link to={DictamenRoute.to}><NavbarItem>Dictamen Tecnológico</NavbarItem></Link>
+                </div>
+                <div>
+                    <NavbarItem>{user.name}</NavbarItem>
+                </div>
             </div>
         </div>
     )
