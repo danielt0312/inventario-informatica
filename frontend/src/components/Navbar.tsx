96f764dbdb393498d6ee897ffee399f72d2df0ac
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-03-24 16:00:10 -0600

    correccion de visualizacion de navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,14 +8,16 @@ function Navbar() {
     const { user } = AuthRoute.useRouteContext()
 
     return (
-        <div className="bg-stone-900">
-            <div className="container flex flex-row">
-                <div className="flex-1">
-                    <Link to={InventarioRoute.to}><NavbarItem>Inventario</NavbarItem></Link>
-                    <Link to={DictamenRoute.to}><NavbarItem>Dictamen Tecnológico</NavbarItem></Link>
-                </div>
-                <div>
-                    <NavbarItem>{user.name}</NavbarItem>
+        <div className="bg-stone-900 flex justify-center">
+            <div className="container">
+                <div className="flex flex-row">
+                    <div className="flex-1">
+                        <Link to={InventarioRoute.to}><NavbarItem>Inventario</NavbarItem></Link>
+                        <Link to={DictamenRoute.to}><NavbarItem>Dictamen Tecnológico</NavbarItem></Link>
+                    </div>
+                    <div>
+                        <NavbarItem>{user.name}</NavbarItem>
+                    </div>
                 </div>
             </div>
         </div>
