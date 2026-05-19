7172a669333efa42016c3f950d9016767a55e0bb
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-05-19 12:12:17 -0600

    feat(components): [new] `query-datatable`

---
+++ frontend/src/components/custom/query-datatable.tsx
@@ -0,0 +1,51 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import { usePagination, useTable } from "./datatable";
+import { DataTable } from "../custom/datatable";
+import { usePaginatedQuery, type UsePaginatedQueryOptions } from "@/hooks/use-paginated-query";
+import type { UseQueryOptions } from "@tanstack/react-query";
+import type { PaginatedResponse } from "@/lib/types";
+import type { DataTableProps } from "../composed/datatable";
+
+export interface QueryDataTableProps<TData, TFilters extends object>
+  extends Omit<DataTableProps<TData>, 'table' | 'query'>,
+          Pick<UsePaginatedQueryOptions<TData, TFilters>, 'queryKey' | 'url' | 'filters' | 'paramsTransformer'> {
+    columns: ColumnDef<TData>[];
+    queryOptions?: Omit<UseQueryOptions<PaginatedResponse<TData>>, "queryKey" | "queryFn">;
+}
+
+export function QueryDataTable<TData, TFilters extends object>({
+    queryKey,
+    filters,
+    columns,
+    url,
+    paramsTransformer,
+    queryOptions,
+    ...params
+}: QueryDataTableProps<TData, TFilters>) {
+    const [pagination, setPagination] = usePagination();
+
+    const query = usePaginatedQuery<TData, TFilters>({
+        queryKey,
+        url,
+        filters,
+        pagination,
+        paramsTransformer,
+        ...queryOptions
+    });
+
+    const table = useTable({
+        data: query.data?.data ?? [],
+        columns,
+        onPaginationChange: setPagination,
+        rowCount: query.data?.total ?? 0,
+        state: { pagination },
+    });
+
+    return (
+        <DataTable
+            table={table}
+            query={query}
+            {...params}
+        />
+    );
+}
