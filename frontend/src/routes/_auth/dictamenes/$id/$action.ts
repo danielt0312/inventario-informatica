61edfd8d72b8da4acaa3dd744e79e9ce46734fb8
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-05-28 15:40:50 -0600

    fix(dictamenes): imports y uso de constants

--- frontend/src/routes/_auth/dictamenes/$id/$action.ts
+++ frontend/src/routes/_auth/dictamenes/$id/$action.ts
@@ -1,7 +1,7 @@
 import api from '@/lib/axios';
-import { DictamenEstado } from '@/lib/constants';
+import { DictamenEstadoEnum } from '@/lib/constants';
 import type { TResponse } from '@/lib/types';
-import type { Dictamen } from '@/views/dictamenes/partials/table.cols';
+import type { Dictamen } from '@/views/dictamenes/partials/table-cols';
 import { View } from '@/views/dictamenes/update';
 import { createFileRoute } from '@tanstack/react-router';
 import z from 'zod';
@@ -10,11 +10,11 @@ export const ACTIONS = ['dictaminar', 'evidenciar', 'facturar', 'inventariar'] a
 export type Action = (typeof ACTIONS)[number];
 
 export const ACTION_TO_ESTADO = {
-    dictaminar: DictamenEstado.POR_DICTAMINAR,
-    evidenciar: DictamenEstado.REQUISITADO,
-    facturar: DictamenEstado.POR_SURTIR,
-    inventariar: DictamenEstado.SURTIDO,
-} as const satisfies Record<Action, DictamenEstado>;
+    dictaminar: DictamenEstadoEnum.POR_DICTAMINAR,
+    evidenciar: DictamenEstadoEnum.REQUISITADO,
+    facturar: DictamenEstadoEnum.POR_SURTIR,
+    inventariar: DictamenEstadoEnum.SURTIDO,
+} as const satisfies Record<Action, DictamenEstadoEnum>;
 
 export const Route = createFileRoute('/_auth/dictamenes/$id/$action')({
     params: {
