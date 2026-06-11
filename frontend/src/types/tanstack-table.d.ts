40c3095ec84f9c91b46253aef773ae6ba6963b13
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-06-11 09:32:25 -0600

    chore(frontend): modulo para inclusion de `TableMeta`

---
+++ frontend/src/types/tanstack-table.d.ts
@@ -0,0 +1,8 @@
+import '@tanstack/react-table';
+
+declare module '@tanstack/react-table' {
+    interface TableMeta<TData> {
+        previewFile?: (uuid: string, title?: string) => void;
+        isPreviewing?: boolean;
+    }
+}
