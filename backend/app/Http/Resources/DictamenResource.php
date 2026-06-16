9a6df7675b3f9ae3f798f4f0acafb885b630283d
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-06-16 09:23:42 -0600

    chore: actualizar respuesta de datos de `DictamenResource`

--- backend/app/Http/Resources/DictamenResource.php
+++ backend/app/Http/Resources/DictamenResource.php
@@ -5,15 +5,13 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Models\DictamenProducto;
-
 class DictamenResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
             'uuid' => $this->uuid,
-            'fecha_solicitud' => $this->fecha_solicitud,
+            'fecha_solicitud' => $this->fecha_solicitud->format('Y-m-d'),
             'adscripcion_id' => $this->adscripcion_id,
             'productos' => DictamenProductoResource::collection($this->whenLoaded('productos')),
             'estado' => new DictamenEstadoResource($this->whenLoaded('estado')),
