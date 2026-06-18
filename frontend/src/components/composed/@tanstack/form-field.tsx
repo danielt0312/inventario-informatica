01a9a55a9c1a7173225e499f3cf6922b2bbd73d1
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-06-18 16:05:48 -0600

    refactor(components): reestructurar `composed/@tanstack/form-field` por legibilidad

--- frontend/src/components/composed/@tanstack/form-field.tsx
+++ frontend/src/components/composed/@tanstack/form-field.tsx
@@ -1,34 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { SaveIcon } from "lucide-react";
+import {
+    useFieldContext,
+    useFormContext
+} from "./form";
+import { Spinner } from "@/components/ui/spinner";
+import { cn, fromISO } from "@/lib/utils";
 import {
     FormField,
-    type FormFieldProps
-} from '@/components/composed/form-field';
-import { useFieldContext, useFormContext } from './form';
-import { Input } from '@/components/ui/input';
-import { CreatableCombobox, type CreatableComboboxProps } from '../combobox-creatable';
-import { Button } from '@/components/ui/button';
-import { Spinner } from '@/components/ui/spinner';
-import { Save } from 'lucide-react';
-import type { ComponentProps } from 'react';
-import { cn, fromISO } from '@/lib/utils';
-import { Checkbox } from '@/components/ui/checkbox';
-import { FieldLabel } from '@/components/ui/field';
-import { DatePicker, type DatePickerProps } from '../date-picker';
-import React from 'react';
-import { FileUploader } from '../file-uploader';
-import type { FileUploadProps } from '@/components/ui/file-upload';
-import { Textarea } from '@/components/ui/textarea';
-import { type CheckedState } from '@radix-ui/react-checkbox';
+    type FormFieldProps as TFormFieldProps
+} from "../form-field";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { FieldLabel } from "@/components/ui/field";
+import { DatePicker } from "../date-picker";
+import React from "react";
+import { FileUploader } from "../file-uploader";
+import { Textarea } from "@/components/ui/textarea";
+import { CreatableCombobox } from "../creatable-combobox";
 
-export interface SubmitButtonProps extends
-    Omit<ComponentProps<typeof Button>, 'type' | 'disabled' | 'children'> {
-    icon?: React.ReactNode;
-    label?: string;
-    children?: (isSubmitting: boolean) => React.ReactNode
-}
-
+export type SubmitButtonProps = &
+    Omit<
+        React.ComponentProps<typeof Button>,
+        'type' | 'disabled' | 'children'
+    > & {
+        icon?: React.ReactNode;
+        label?: string;
+        children?: (isSubmitting: boolean) => React.ReactNode;
+    }
 export const SubmitButton = ({
     label = "Guardar",
-    icon = <Save />,
+    icon = <SaveIcon />,
     className,
     children,
     ...props
@@ -57,13 +59,17 @@ export const SubmitButton = ({
                         className={cn("max-w-fit self-center", className)}
                         {...props}
                     />
-                )
+                );
             }}
         </form.Subscribe>
     );
 }
 
+export type FormFieldProps = Omit<TFormFieldProps, 'orientation'> & {
+    fieldOrientation?: TFormFieldProps['orientation'];
+};
 export const Field = ({
+    fieldOrientation,
     ...props
 }: FormFieldProps) => {
     const field = useFieldContext();
@@ -71,27 +77,35 @@ export const Field = ({
     return (
         <FormField
             errors={field.state.meta.errors}
+            orientation={fieldOrientation}
             {...props}
         />
     );
 };
 
-export type OmitProps = Omit<
+export type OmitGenericProps = Omit<
     FormFieldProps,
     "children" | "onChange" | "onBlur" | "onFocus" | "onClick"
 >;
 
-export type FormTextFieldProps = OmitProps & React.ComponentProps<typeof Input>;
+export type TextFieldProps = OmitGenericProps & React.ComponentProps<typeof Input>;
 export const TextField = ({
     type,
     label,
+    disabled,
+    fieldOrientation,
     className,
     ...props
-}: FormTextFieldProps) => {
-    const field = useFieldContext<typeof type>();
+}: TextFieldProps) => {
+    const field = useFieldContext<TextFieldProps['value']>();
 
     return (
-        <Field className={className} label={label}>
+        <Field
+            label={label}
+            data-disabled={disabled}
+            fieldOrientation={fieldOrientation}
+            className={className}
+        >
             <Input
                 type={type}
                 placeholder="Ingresa un valor"
@@ -103,44 +117,59 @@ export const TextField = ({
     );
 };
 
-export type CreatableComboboxFieldProps =
-    OmitProps &
-    CreatableComboboxProps
+export type CreatableComboboxFieldProps = OmitGenericProps & React.ComponentProps<typeof CreatableCombobox>;
 export const CreatableComboboxField = ({
     label,
-    enabled,
+    disabled,
+    fieldOrientation,
+    className,
     ...props
 }: CreatableComboboxFieldProps) => {
-    const field = useFieldContext<CreatableComboboxProps['value']>();
+    const field = useFieldContext<CreatableComboboxFieldProps['value']>();
 
     return (
-        <Field label={label} data-disabled={enabled}>
+        <Field
+            label={label}
+            data-disabled={disabled}
+            fieldOrientation={fieldOrientation}
+            className={className}
+        >
             <CreatableCombobox
                 value={field.state.value}
                 onValueChange={field.handleChange}
-                enabled={enabled}
+                disabled={disabled}
                 {...props}
             />
         </Field>
     );
 };
 
-export type CheckboxFieldProps =
-    OmitProps &
-    ComponentProps<typeof Checkbox> & {
-        enabled?: boolean;
-    };
+export type TOmitCreatableComboboxFieldsProps<
+    O extends keyof CreatableComboboxFieldProps = never
+> = Omit<
+    CreatableComboboxFieldProps, 'options' | O
+>;
+
+export type CheckboxFieldProps = OmitGenericProps & React.ComponentProps<typeof Checkbox>;
 export const CheckboxField = ({
     label,
-    enabled,
+    disabled,
+    className,
+    fieldOrientation = "horizontal",
     ...props
 }: CheckboxFieldProps) => {
-    const field = useFieldContext<CheckedState>();
+    const field = useFieldContext<CheckboxFieldProps['checked']>();
 
     return (
-        <Field data-disabled={enabled} orientation="horizontal">
+        <Field
+            label={label}
+            data-disabled={disabled}
+            fieldOrientation={fieldOrientation}
+            className={className}
+        >
             <Checkbox
                 onCheckedChange={field.handleChange}
+                disabled={disabled}
                 {...props}
             />
             <FieldLabel>{label}</FieldLabel>
@@ -148,45 +177,53 @@ export const CheckboxField = ({
     );
 }
 
-export type DatePickerFieldProps<T extends Date | string = Date> =
-    OmitProps &
-    DatePickerProps & {
-        parseValue?: (d: Date | undefined) => T
-    };
+export type DatePickerFieldProps<T extends Date | string = Date> = OmitGenericProps & React.ComponentProps<typeof DatePicker> & {
+    parseValue?: (d: Date | undefined) => T
+};
 export const DatePickerField = <T extends Date | string = Date>({
     label,
+    className,
+    fieldOrientation,
+    disabled,
     parseValue = (d) => d as T,
     ...props
 }: DatePickerFieldProps<T>) => {
-    const field = useFieldContext<T>()
+    const field = useFieldContext<T>();
 
     const dateValue = React.useMemo(() => {
-        const v = field.state.value
-        if (!v) return undefined
-        if (v instanceof Date) return v
-        return fromISO(v)
-    }, [field.state.value])
+        const v = field.state.value;
+        if (!v) return undefined;
+        if (v instanceof Date) return v;
+        return fromISO(v);
+    }, [field.state.value]);
 
     return (
-        <Field label={label}>
+        <Field
+            label={label}
+            data-disabled={disabled}
+            fieldOrientation={fieldOrientation}
+            className={className}
+        >
             <DatePicker
                 value={dateValue}
                 onValueChange={(d) => field.handleChange(parseValue(d))}
                 {...props}
             />
         </Field>
-    )
+    );
 }
 
-export type FileUploaderFieldProps = OmitProps & FileUploadProps;
+export type FileUploaderFieldProps = OmitGenericProps & React.ComponentProps<typeof FileUploader>;
 export const FileUploaderField = ({
     label,
+    className,
+    fieldOrientation,
     ...props
 }: FileUploaderFieldProps) => {
     const field = useFieldContext<FileUploaderFieldProps['value']>();
 
     return (
-        <Field label={label}>
+        <Field label={label} className={className} fieldOrientation={fieldOrientation}>
             <FileUploader
                 value={field.state.value}
                 onValueChange={field.handleChange}
@@ -196,16 +233,17 @@ export const FileUploaderField = ({
     );
 }
 
-export type TextareaFieldProps = OmitProps & ComponentProps<typeof Textarea>;
+export type TextareaFieldProps = OmitGenericProps & React.ComponentProps<typeof Textarea>;
 export const TextareaField = ({
     label,
     className,
+    fieldOrientation,
     ...props
 }: TextareaFieldProps) => {
     const field = useFieldContext<TextareaFieldProps['value']>();
 
     return (
-        <Field label={label} className={className}>
+        <Field label={label} className={className} fieldOrientation={fieldOrientation}>
             <Textarea
                 value={field.state.value}
                 onChange={(e) => field.handleChange(e.target.value)}
