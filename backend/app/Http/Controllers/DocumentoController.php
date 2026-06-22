d0bccbfa38415391c69ad012c3214f44b2c8dba1
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-06-22 10:31:27 -0600

    chore(documentos): implementar `Spatie/QueryBuilder` en endpoint `GET`

--- backend/app/Http/Controllers/DocumentoController.php
+++ backend/app/Http/Controllers/DocumentoController.php
@@ -2,27 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{Documento, Archivo};
+use App\Models\Documento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Requests\Documento\{DocumentoRequest, StoreDocumentoRequest};
-use App\Enums\AvailableFileExtensions;
+use Spatie\QueryBuilder\{AllowedFilter, QueryBuilder};
 
 class DocumentoController extends Controller
 {
-    public function index(DocumentoRequest $request)
+    public function index(Request $request)
     {
-        $query = Documento::with(['tipo']);
-
-        if ($request->filled('archivo')) {
-            $query->whereHas('archivo', fn ($q)
-                => $q->whereLike('nombre', "%{$request->input('archivo')}%"));
-        }
-
-        if ($request->filled('tipos')) {
-            $query->whereHas('tipo', fn ($q)
-                => $q->whereIn('id', $request->input('tipos')));
-        }
+        $query = QueryBuilder::for(Documento::class)
+            ->with('tipo')
+            ->allowedFilters(
+                AllowedFilter::exact('tipos', 'tipo_id'),
+                AllowedFilter::partial('archivo', 'archivo.nombre')
+            );
 
         return $query
             ->paginate($request->query('per_page', 10))
