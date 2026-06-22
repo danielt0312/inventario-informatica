8d6fb050a477875b1edc89ae64b3c1b6544c4252
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-06-22 10:10:24 -0600

    fix(hooks): usar por default `spatieFilterTransformer` en `use-paginated-query`

--- frontend/src/hooks/use-paginated-query.ts
+++ frontend/src/hooks/use-paginated-query.ts
@@ -1,37 +1,34 @@
 import api from "@/lib/axios";
+import { spatieFilterTransformer } from "@/lib/utils";
 import type { PaginatedResponse } from "@/types/generics";
 import { useQuery, type UseQueryOptions, type UseQueryResult } from "@tanstack/react-query";
 import type { PaginationState } from "@tanstack/react-table";
 
 export interface UsePaginatedQueryOptions<TData, TFilters extends object = Record<string, unknown>>
-    extends Omit<UseQueryOptions<PaginatedResponse<TData>>, "queryFn"> {
+    extends Omit<UseQueryOptions<PaginatedResponse<TData>>, 'queryFn'> {
     url: string;
-    filters?: TFilters;
+    filter?: TFilters;
     pagination: PaginationState;
-    paramsTransformer?: (filters?: TFilters) => Record<string, unknown>;
+    filterTransformer?: (filters?: TFilters) => Record<string, unknown>;
 }
 
-export function usePaginatedQuery<TData, TFilters extends object>({
+export function usePaginatedQuery<TData, TFilters extends object = Record<string, unknown>>({
     queryKey,
     url,
-    filters,
+    filter,
     pagination,
-    paramsTransformer,
+    filterTransformer = spatieFilterTransformer,
     ...params
 }: UsePaginatedQueryOptions<TData, TFilters>): UseQueryResult<PaginatedResponse<TData>> {
     return useQuery({
-        queryKey: [...queryKey, filters ?? {}, pagination],
-        queryFn: () => {
-            const transformedParams = paramsTransformer ? paramsTransformer(filters) : filters;
-
-            return api.get<PaginatedResponse<TData>>(url, {
-                params: {
-                    ...transformedParams,
-                    page: pagination.pageIndex + 1,
-                    per_page: pagination.pageSize,
-                },
-            }).then(r => r.data)
-        },
+        queryKey: [...queryKey, filter ?? {}, pagination],
+        queryFn: () => api.get<PaginatedResponse<TData>>(url, {
+            params: {
+                ...filterTransformer(filter),
+                page: pagination.pageIndex + 1,
+                per_page: pagination.pageSize,
+            },
+        }).then(r => r.data),
         ...params
     });
 }
