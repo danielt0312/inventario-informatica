8a641ccb885dc7b97fcc7557a41463e7b62756e7
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-07-07 14:52:39 -0600

    feat(components): agregar fallbacks para fields `composed/@tanstack/form/[radio-group|text-area]`

--- frontend/src/components/composed/@tanstack/form/radio-group-field.tsx
+++ frontend/src/components/composed/@tanstack/form/radio-group-field.tsx
@@ -1,14 +1,35 @@
 import * as Root from "../../radio-group-field";
 import { useFieldContext } from "./form";
 
-export const RadioGroupField = (props: Root.RadioGroupFieldProps) => {
-    const field = useFieldContext<Root.RadioGroupFieldItemProps['value']>();
+export type RadioGroupField = string | number | boolean | undefined;
+export const RadioGroupField = ({
+    value,
+    onValueChange,
+    ...props
+}: Root.RadioGroupFieldProps) => {
+    const field = useFieldContext<RadioGroupField>();
 
     return (
         <Root.RadioGroupField
             name={field.name}
-            value={field.state.value}
-            onValueChange={field.handleChange}
+            value={value !== undefined
+                ? value
+                : field.state.value !== undefined
+                    ? String(field.state.value)
+                    : ''
+            }
+            onValueChange={(value) => onValueChange !== undefined
+                ? onValueChange(value)
+                : field.handleChange(
+                    value === ''
+                        ? undefined
+                        : value === 'true' || value === 'false'
+                            ? value === 'true'
+                            : typeof field.state.value === 'number' || !isNaN(Number(value)) && value !== null
+                                ? Number(value)
+                                : value
+                )
+            }
             errors={field.state.meta.errors}
             {...props}
         />
--- frontend/src/components/composed/@tanstack/form/textarea-field.tsx
+++ frontend/src/components/composed/@tanstack/form/textarea-field.tsx
@@ -1,16 +1,61 @@
 import { useFieldContext } from "./form"
 import * as Root from "../../textarea-field";
 
-export const TextareaField = (props: Root.TextareaFieldProps) => {
-    const field = useFieldContext<Root.TextareaFieldProps['value']>()
+export type TextareaField = string | undefined;
+export const TextareaField = ({
+    value,
+    onChange,
+    ...props
+}: Root.TextareaFieldProps) => {
+    const field = useFieldContext<TextareaField>();
 
     return (
         <Root.TextareaField
             name={field.name}
-            value={field.state.value}
-            onChange={(e) => field.handleChange(e.target.value)}
+            value={value !== undefined
+                ? value
+                : field.state.value === undefined
+                    ? ''
+                    : field.state.value
+            }
+            onChange={(e) => onChange !== undefined
+                ? onChange(e)
+                : field.handleChange(
+                    e.target.value.trim() === ''
+                        ? undefined
+                        : e.target.value.trim()
+                )
+            }
             errors={field.state.meta.errors}
             {...props}
         />
     );
 }
+
+export type NullableTextareaField = TextareaField | null;
+export const NullableTextareaField = ({
+    value,
+    onChange,
+    ...props
+}: React.ComponentProps<typeof TextareaField>) => {
+    const field = useFieldContext<NullableTextareaField>();
+
+    return (
+        <TextareaField
+            value={
+                value !== undefined
+                    ? value
+                    : field.state.value === null
+                        ? ''
+                        : undefined
+            }
+            onChange={(e) => onChange !== undefined
+                ? onChange(e)
+                : e.target.value.trim() === ''
+                ? field.handleChange(null)
+                : undefined
+            }
+            {...props}
+        />
+    )
+};
