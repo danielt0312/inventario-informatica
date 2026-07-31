caf73362f650d866c7fb7054fe3088de5c3c7776
danielt0312 <torrescoloradodaniel@gmail.com>
2026-07-31 16:09:11 -0600

    feat(components): [new] `composed/link-to-file`

---
+++ frontend/src/components/composed/link-to-file.tsx
@@ -0,0 +1,40 @@
+import { useFilePreviewWindowMutation, type FilePreviewWindowMutationFn } from "@/hooks/use-file-preview-window-mutation";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Label } from "../ui/label";
+import { EyeIcon, type LucideProps } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export const LinkToFile = ({
+    title,
+    uuid,
+    label,
+    className,
+    iconSize = 14,
+    tooltipMessage = 'Ver documento',
+    ...props
+}: FilePreviewWindowMutationFn & Omit<React.ComponentProps<typeof Label>, 'title' | 'children' | 'onClick'> & {
+    iconSize?: LucideProps['size'];
+    label: string;
+    tooltipMessage?: string;
+}) => {
+    const { mutateAsync } = useFilePreviewWindowMutation();
+
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Label
+                    onClick={async () => {
+                        await mutateAsync({ uuid, title });
+                    }}
+                    className={cn("text-sm gap-1 text-blue-400 underline-offset-4 hover:underline w-fit justify-self-start", className)}
+                    {...props}
+                >
+                    <EyeIcon size={iconSize} />{label}
+                </Label>
+            </TooltipTrigger>
+            <TooltipContent>
+                {tooltipMessage}
+            </TooltipContent>
+        </Tooltip>
+    );
+}
