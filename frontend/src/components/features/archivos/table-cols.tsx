0039aaf235e15d37b3d73016023fd64c961336e6
danielt0312 <torrescoloradodaniel@gmail.com>
2026-08-18 21:43:42 -0600

    chore: dejar de usar `action-menu` para acciones de columnas en tablas

--- frontend/src/components/features/archivos/table-cols.tsx
+++ frontend/src/components/features/archivos/table-cols.tsx
@@ -1,4 +1,5 @@
-import { ActionMenu, ActionMenuItem } from "@/components/ui/action-menu";
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { toLocaleDateFormat } from "@/lib/utils";
 import type { Archivo } from "@/types/documentos";
 import type { TRowDataAccessFn } from "@/types/generics";
@@ -8,20 +9,20 @@ import { EyeIcon } from "lucide-react";
 type RowDataAccessFn<TRowData> = TRowDataAccessFn<TRowData, Archivo>;
 
 const NombreRow = <TRowData,>(getRowData: RowDataAccessFn<TRowData>): ColumnDef<TRowData> => ({
-    id: 'archivo.nombre',
+    id: 'nombre',
     header: 'Nombre del Archivo',
     accessorFn: (row) => getRowData(row).nombre
 });
 
 const FechaSubidaRow = <TRowData,>(getRowData: RowDataAccessFn<TRowData>): ColumnDef<TRowData> => ({
-    id: 'archivo.fecha_subida',
+    id: 'fecha_subida',
     header: 'Fecha de Subida',
     accessorFn: (row) => toLocaleDateFormat(getRowData(row).created_at, { hour: '2-digit' })
 });
 
 function PreviewActionRow<TRowData>({
     archivo,
-    meta
+    meta,
 }: {
     archivo: Archivo,
     meta?: TableMeta<TRowData>;
@@ -29,21 +30,26 @@ function PreviewActionRow<TRowData>({
     const { uuid, nombre } = archivo;
 
     return (
-        <ActionMenuItem
-            disabled={meta?.isPreviewing}
-            onClick={() => meta?.previewFile?.(uuid, nombre)}
-        >
-            <EyeIcon /> Ver documento
-        </ActionMenuItem>
+        <Tooltip>
+            <TooltipContent>Ver Documento</TooltipContent>
+            <TooltipTrigger asChild>
+                <Button
+                    disabled={meta?.isPreviewing}
+                    onClick={() => meta?.previewFile?.(uuid, nombre)}
+                    variant="outline"
+                    size="icon"
+                >
+                    <EyeIcon />
+                </Button>
+            </TooltipTrigger>
+        </Tooltip>
     );
 }
 
 const ActionRow = <TRowData,>(getRowData: RowDataAccessFn<TRowData>): ColumnDef<TRowData> => ({
-    id: 'archivo.actions',
+    id: 'actions',
     cell: ({ row, table }) => (
-        <ActionMenu>
-            <PreviewActionRow meta={table.options.meta} archivo={getRowData(row.original)} />
-        </ActionMenu>
+        <PreviewActionRow meta={table.options.meta} archivo={getRowData(row.original)} />
     ),
 });
 
@@ -54,7 +60,7 @@ const getDefaultColumns = <TRowData,>(getRowData: RowDataAccessFn<TRowData>): Co
 ]);
 
 const initialState: InitialTableState = {
-    columnOrder: ['archivo.nombre', 'archivo.fecha_subida', 'archivo.actions'],
+    columnOrder: ['nombre', 'fecha_subida', 'actions'],
 }
 
 export { type RowDataAccessFn as ArchivoRowDataAccessFn, getDefaultColumns as getArchivoDefaultColumns, NombreRow as ArchivoNombreRow, FechaSubidaRow as ArchivoFechaSubidaRow, ActionRow as ArchivoActionRow, PreviewActionRow as ArchivoPreviewActionRow, initialState as archivoTableInitialState }
