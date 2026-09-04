effc1f150d9816c5e09538aaac541410a1ae2f08
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-09-04 12:25:34 -0600

    fix(resguardos): mostrar articulos resguardados en UI

--- frontend/src/components/features/resguardos/create/form.tsx
+++ frontend/src/components/features/resguardos/create/form.tsx
@@ -2,11 +2,11 @@ import type { Articulo } from "@/types/articulos";
 import type { TResponse } from "@/types/generics";
 import type { DetailedResguardo, ResguardoArticulo } from "@/types/resguardos";
 import { Form } from "@/components/ui/form";
-import { useAppForm } from "@/components/ui/form-context";
-import { EmpleadoField } from "../../externos/empleados/form-fields";
+import { useAppForm, useFieldContext } from "@/components/ui/form-context";
+import { EmpleadoField, type EmpleadoFieldType } from "../../externos/empleados/form-fields";
 import { createResguardoDefaultValues, createResguardoValidator, type CreateResguardoSchemaOutput } from "./form-schema";
 import { DatePicker } from "@/components/ui/date-picker";
-import { FieldGroup } from "@/components/ui/field";
+import { FieldError, FieldGroup } from "@/components/ui/field";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { BaseFieldLayout } from "@/components/ui/field-layout";
@@ -14,7 +14,6 @@ import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { DataTable } from "@/components/ui/datatable";
 import { articuloDefaultColumnsBuilder } from "@/views/articulos/partials/table-cols";
 import { toLocaleDateFormat } from "@/lib/utils";
-import { useStore } from "@tanstack/react-form";
 import { useQuery } from "@tanstack/react-query";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAppQueryDataTable } from "@/components/ui/query-datatable";
@@ -27,9 +26,9 @@ import { useNavigate } from "@tanstack/react-router";
 import { Route as ResguardoIndexRoute } from "@/routes/_auth/resguardos";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { SubmitButton } from "@/components/ui/submit-button";
+import { Skeleton } from "@/components/ui/skeleton";
 import api from "@/lib/axios";
 import React from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 
 type ArticuloResguardado = ResguardoArticulo<Articulo> | {
     id?: undefined;
@@ -68,192 +67,202 @@ function CreateForm() {
 
     const [alertOpen, setAlertOpen] = React.useState(false);
 
-    const empleadoId = useStore(form.store, (state) => state.values.empleado_id);
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Resguardo de Bienes Informáticos</CardTitle>
+            </CardHeader>
 
-    function ResguardoDetalle() {
-        const { data: resguardoActual, isLoading } = useQuery({
-            queryKey: ['resguardos', empleadoId],
-            queryFn: () => api.get<TResponse<DetailedResguardo<Articulo> | null>>(`api/empleados/${empleadoId}/resguardo`, {
-                params: {
-                    include: [
-                        'articulosResguardados.articulo.producto.marca',
-                        'articulosResguardados.articulo.producto.tipo.categoria',
-                        'articulosResguardados.articulo.estado',
-                    ]
-                }
-            }).then(r => r.data.data)
-        });
+            <CardContent>
+                <Form form={form} className="flex flex-col gap-7">
+                    <form.AppForm>
+                        <FieldGroup className="flex-row">
+                            <form.AppField
+                                name="empleado_id"
+                                children={() => <EmpleadoField required />}
+                            />
 
-        if (isLoading) return <TableSkeleton />;
+                            <BaseFieldLayout label="Fecha de actualización" required disabled>
+                                <DatePicker value={new Date} disabled />
+                            </BaseFieldLayout>
+                        </FieldGroup>
 
-        return (
-            <ResguardoTablas articulosResguardadosPrevios={resguardoActual?.articulos_resguardados ?? []} />
-        );
-    }
+                        <form.Subscribe selector={(state) => state.values.empleado_id}>
+                            {(empleadoId) => empleadoId && (
+                                <form.AppField
+                                    name="articulos"
+                                    children={() => <ResguardoDetalle key={empleadoId} empleadoId={empleadoId} />}
+                                />
+                            )}
+                        </form.Subscribe>
 
-    function ResguardoTablas({ articulosResguardadosPrevios }: { articulosResguardadosPrevios: ArticuloResguardado[]; }) {
-        const [articulosResguardados, setArticulosResguardados] = React.useState(articulosResguardadosPrevios);
-
-        const articulosResguardadosIds = React.useMemo(
-            () => new Set(articulosResguardados.map(ar => ar.articulo.uuid)),
-            [articulosResguardados]
-        );
-
-        React.useEffect(() => {
-            form.setFieldValue('articulos', articulosResguardados.map(ar => ar.articulo.uuid));
-        }, [articulosResguardados]);
-
-        const formTable = useReactTable({
-            data: articulosResguardados,
-            columns: [
-                {
-                    header: "Fecha de Asignación",
-                    cell: ({ row: { original } }) => (
-                        original.id !== undefined
-                            ? toLocaleDateFormat(original.fecha_asignacion)
-                            : <span className="italic text-muted-foreground">N/A</span>
-                    )
-                },
-                ...articuloDefaultColumnsBuilder<ArticuloResguardado>(row => row.articulo),
-                {
-                    id: "actions",
-                    cell: ({ row }) => (
                         <Button
-                            size="sm"
-                            variant="destructive"
-                            onClick={() => {
-                                const articuloActual = row.original;
-                                setArticulosResguardados(prev => prev.filter(item => item !== articuloActual));
+                            className="self-center"
+                            onClick={async () => {
+                                form.validateSync('submit');
+                                await form.validateAsync('submit');
+                                if (!form.state.isValid) return;
+                                setAlertOpen(true);
                             }}
                         >
-                            <Trash2Icon /> Eliminar
+                            <SaveIcon /> Guardar
                         </Button>
-                    )
-                }
-            ],
-            getCoreRowModel: getCoreRowModel()
-        });
 
-        function ArticulosTable() {
-            const [tablePagination, setTablePagination] = usePagination();
+                        <AlertDialog open={alertOpen} onOpenChange={setAlertOpen}>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>¿Deseas continuar?</AlertDialogTitle>
+                                    <AlertDialogDescription>Verifica que la información sea correcta antes de continuar</AlertDialogDescription>
+                                </AlertDialogHeader>
 
-            const { data, isLoading } = usePaginatedQuery<Articulo>({
-                url: 'api/articulos',
-                queryKey: ['articulos'],
-                pagination: tablePagination,
-            });
+                                <AlertDialogFooter>
+                                    <AlertDialogAction asChild>
+                                        <SubmitButton isSubmitting={isPending} onClick={() => form.handleSubmit()} />
+                                    </AlertDialogAction>
 
-            const table = useAppQueryDataTable({
-                data: data?.data ?? [],
-                columns: [
-                    ...articuloDefaultColumnsBuilder<Articulo>(row => row),
-                    {
-                        id: "selector",
-                        cell: ({ row }) => {
-                            const articulo = row.original;
-                            const yaResguardado = articulosResguardadosIds.has(articulo.uuid);
-                            return (
-                                <Button
-                                    size="sm"
-                                    variant={yaResguardado ? 'secondary' : 'default'}
-                                    disabled={yaResguardado}
-                                    onClick={() => setArticulosResguardados((prev) => [...prev, { articulo }])}
-                                >
-                                    {yaResguardado ? (
-                                        <><CheckCircleIcon /> Agregado</>
-                                    ) : (
-                                        <><CirclePlusIcon /> Agregar</>
-                                    )}
-                                </Button>
-                            );
-                        }
-                    }
-                ],
-                rowCount: data?.meta.total ?? 0,
-                state: { pagination: tablePagination },
-                onPaginationChange: setTablePagination
-            });
+                                    <AlertDialogCancel>
+                                        <CircleXIcon /> Cancelar
+                                    </AlertDialogCancel>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                    </form.AppForm>
+                </Form>
+            </CardContent>
+        </Card>
+    );
+}
 
-            if (isLoading) return <TableSkeleton />;
+function ResguardoDetalle({ empleadoId }: { empleadoId: EmpleadoFieldType }) {
+    const { data: resguardoActual, isLoading } = useQuery({
+        queryKey: ['resguardos', empleadoId],
+        queryFn: () => api.get<TResponse<DetailedResguardo<Articulo> | null>>(`api/empleados/${empleadoId}/resguardo`, {
+            params: {
+                include: [
+                    'articulosResguardados.articulo.producto.marca',
+                    'articulosResguardados.articulo.producto.tipo.categoria',
+                    'articulosResguardados.articulo.estado',
+                ]
+            }
+        }).then(r => r.data.data)
+    });
 
-            return (
-                <>
-                    <Label className="font-bold text-lg">Articulos disponibles para resguardo</Label>
-                    <DataTableLayout table={table} />
-                </>
-            );
-        }
+    if (isLoading) return <TableSkeleton />;
 
+    return (
+        <ResguardoTablas articulosResguardadosPrevios={resguardoActual?.articulos_resguardados ?? []} />
+    );
+}
 
-        return (
-            <>
-                <Label className="font-bold text-lg">Bienes Informáticos Resguardados</Label>
-                <DataTable table={formTable} />
+function ResguardoTablas({ articulosResguardadosPrevios }: { articulosResguardadosPrevios: ArticuloResguardado[]; }) {
+    const field = useFieldContext<string[] | undefined>();
 
-                <ArticulosTable />
-            </>
-        );
-    }
+    const [articulosResguardados, setArticulosResguardados] = React.useState(articulosResguardadosPrevios);
+
+    const articulosResguardadosIds = React.useMemo(
+        () => new Set(articulosResguardados.map(ar => ar.articulo.uuid)),
+        [articulosResguardados]
+    );
+
+    React.useEffect(() => {
+        field.setValue(articulosResguardados.map(ar => ar.articulo.uuid));
+    }, [articulosResguardados]);
+
+    const formTable = useReactTable({
+        data: articulosResguardados,
+        columns: [
+            {
+                header: "Fecha de Asignación",
+                cell: ({ row: { original } }) => (
+                    original.id !== undefined
+                        ? toLocaleDateFormat(original.fecha_asignacion)
+                        : <span className="italic text-muted-foreground">N/A</span>
+                )
+            },
+            ...articuloDefaultColumnsBuilder<ArticuloResguardado>(row => row.articulo),
+            {
+                id: "actions",
+                cell: ({ row }) => (
+                    <Button
+                        size="sm"
+                        variant="destructive"
+                        onClick={() => {
+                            const articuloActual = row.original;
+                            setArticulosResguardados(prev => prev.filter(item => item !== articuloActual));
+                        }}
+                    >
+                        <Trash2Icon /> Eliminar
+                    </Button>
+                )
+            }
+        ],
+        getCoreRowModel: getCoreRowModel()
+    });
 
     return (
         <>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Resguardo de Bienes Informáticos</CardTitle>
-                </CardHeader>
+            <Label className="font-bold text-lg">Bienes Informáticos Resguardados</Label>
+            <DataTable table={formTable} />
+            <FieldError errors={field.state.meta.errors} />
 
-                <CardContent>
-                    <Form form={form} className="flex flex-col gap-7">
-                        <form.AppForm>
-                            <FieldGroup className="flex-row">
-                                <form.AppField
-                                    name="empleado_id"
-                                    children={() => <EmpleadoField required />}
-                                />
+            <ArticulosTable
+                articulosResguardadosUuids={articulosResguardadosIds}
+                onAgregar={(articulo) => setArticulosResguardados((prev) => [...prev, { articulo }])}
+            />
+        </>
+    );
+}
 
-                                <BaseFieldLayout label="Fecha de actualización" required disabled>
-                                    <DatePicker value={new Date} disabled />
-                                </BaseFieldLayout>
-                            </FieldGroup>
+function ArticulosTable({
+    articulosResguardadosUuids,
+    onAgregar
+}: {
+    articulosResguardadosUuids: Set<string>;
+    onAgregar: (articulo: Articulo) => void
+}) {
+    const [tablePagination, setTablePagination] = usePagination();
 
-                            {empleadoId && (
-                                <ResguardoDetalle key={empleadoId} />
-                            )}
+    const { data, isLoading } = usePaginatedQuery<Articulo>({
+        url: 'api/articulos',
+        queryKey: ['articulos'],
+        pagination: tablePagination,
+    });
 
-                            <Button
-                                className="self-center"
-                                onClick={async () => {
-                                    form.validateSync('submit');
-                                    await form.validateAsync('submit');
-                                    if (!form.state.isValid) return;
-                                    setAlertOpen(true);
-                                }}
-                            >
-                                <SaveIcon /> Guardar
-                            </Button>
+    const table = useAppQueryDataTable({
+        data: data?.data ?? [],
+        columns: [
+            ...articuloDefaultColumnsBuilder<Articulo>(row => row),
+            {
+                id: "actions",
+                cell: ({ row }) => {
+                    const articulo = row.original;
+                    const yaResguardado = articulosResguardadosUuids.has(articulo.uuid);
+                    return (
+                        <Button
+                            size="sm"
+                            variant={yaResguardado ? 'secondary' : 'default'}
+                            disabled={yaResguardado}
+                            onClick={() => onAgregar(articulo)}
+                        >
+                            {yaResguardado
+                                ? <><CheckCircleIcon /> Agregado</>
+                                : <><CirclePlusIcon /> Agregar</>}
+                        </Button>
+                    );
+                }
+            }
+        ],
+        rowCount: data?.meta.total ?? 0,
+        state: { pagination: tablePagination },
+        onPaginationChange: setTablePagination
+    });
 
-                            <AlertDialog open={alertOpen} onOpenChange={setAlertOpen}>
-                                <AlertDialogContent>
-                                    <AlertDialogHeader>
-                                        <AlertDialogTitle>¿Deseas continuar?</AlertDialogTitle>
-                                        <AlertDialogDescription>Verifica que la información sea correcta antes de continuar</AlertDialogDescription>
-                                    </AlertDialogHeader>
+    if (isLoading) return <TableSkeleton />;
 
-                                    <AlertDialogFooter>
-                                        <AlertDialogAction asChild>
-                                            <SubmitButton isSubmitting={isPending} onClick={() => form.handleSubmit()} />
-                                        </AlertDialogAction>
-
-                                        <AlertDialogCancel>
-                                            <CircleXIcon /> Cancelar
-                                        </AlertDialogCancel>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
-                        </form.AppForm>
-                    </Form>
-                </CardContent>
-            </Card>
+    return (
+        <>
+            <Label className="font-bold text-lg">Articulos disponibles para resguardar</Label>
+            <DataTableLayout table={table} />
         </>
     );
 }
