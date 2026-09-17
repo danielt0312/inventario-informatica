9f1d03af114a317767b34513d5e345b643fbcadd
Daniel Torres <torrescoloradodaniel@gmail.com>
2026-09-17 15:59:57 -0600

    chore(articulos): eliminar estado 'mantenimiento'

--- backend/app/Enums/ArticuloEstadoEnum.php
+++ backend/app/Enums/ArticuloEstadoEnum.php
@@ -11,5 +11,4 @@ enum ArticuloEstadoEnum: int
     case ACTIVO = 1;
     case BAJA_DEFINITIVA = 2;
     case BAJA_PREVENTIVA = 3;
-    case MANTENIMIENTO = 4;
 }
--- frontend/src/views/articulos/partials/table-cols.tsx
+++ frontend/src/views/articulos/partials/table-cols.tsx
@@ -18,7 +18,6 @@ const estadoColorVariants = cva(
                 [ArticuloEstadoEnum.ACTIVO]: "bg-lime-400",
                 [ArticuloEstadoEnum.BAJA]: "bg-red-400",
                 [ArticuloEstadoEnum.BAJA_PREVENTIVA]: "bg-red-400/80",
-                [ArticuloEstadoEnum.MANTENIMIENTO]: "bg-yellow-400/50",
             }
         },
         defaultVariants: {
